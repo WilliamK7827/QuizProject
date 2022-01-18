2eed2804345f8c58479521d4f14e8ff9c46a2408
WilliamK7827 <74074978+WilliamK7827@users.noreply.github.com>
2022-01-18 11:22:41 -0800

    Version3

---
+++ version 3/sw.js
@@ -0,0 +1,55 @@
+const version = 'v1';
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(version).then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/quizproject.css',
+        '/quizproject.js',
+        '/manifest.json',
+        '/notfound.txt',
+        '/icons/icon-192x192.png',
+        '/icons/icon-256x256.png',
+        '/icons/icon-384x384.png',
+        '/icons/icon-512x512.png',
+        '/quizimages/loading.jpg',
+        '/quizimages/q1-1.jpg',
+        '/quizimages/q1-2.jpg',
+        '/quizimages/q1-3.jpg',
+        '/quizimages/q1-4.jpg',
+        '/quizimages/q1-5.jpg',
+        '/quizimages/q1-6.jpg',
+        '/quizimages/q1-7.jpg',
+        '/quizimages/q1-8.jpg',
+        '/quizimages/q1-9.jpg',
+        '/quizimages/q1-10.jpg'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(caches.match(event.request).then(function(response) {
+    // caches.match() always resolves
+    // but in case of success response will have value
+    if (response !== undefined) {
+      return response;
+    } else {
+      return fetch(event.request).then(function (response) {
+        // response may be used only once
+        // we need to save clone to put one copy in cache
+        // and serve second one
+        let responseClone = response.clone();
+
+        caches.open(version).then(function (cache) {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      }).catch(function () {
+        return caches.match('/notfound.txt');
+      });
+    }
+  }));
+});
